4bce9fb1190d01abc45a6a349ac11e037135389c
BV Satyaram <bvsatyaram@gmail.com>
2018-08-26 08:40:45 +0530

    Add attack functionality

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+var AppUtis = {
+  randNumber: function(max) {
+    return Math.floor(Math.random() * max) + 1;
+  }
+}
+
 new Vue({
   el: '#app',
   data: {
     health: {
       me: 100,
       monster: 100
+    }
+  },
+  methods: {
+    attack: function() {
+      this.health.me -= AppUtis.randNumber(10);
+      this.health.monster -= AppUtis.randNumber(10);
     }
   }
 })
