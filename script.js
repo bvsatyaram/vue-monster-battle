63e8840a487f4bb121479303ad46ad90f87adcc1
BV Satyaram <bvsatyaram@gmail.com>
2018-08-26 08:50:26 +0530

    Add heal functionality

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 var AppUtis = {
   randNumber: function(max) {
     return Math.floor(Math.random() * max) + 1;
+  },
+  normalizeNumber: function(num, min, max) {
+    if (num < min) {
+      num = min;
+    } else if (num > max) {
+      num = max;
+    }
+
+    return num;
   }
 }
 
@@ -12,10 +21,24 @@ new Vue({
       monster: 100
     }
   },
+  computed: {
+    normalizedHealth: function() {
+      this.health.me = AppUtis.normalizeNumber(this.health.me, 0, 100);
+      this.health.monster = AppUtis.normalizeNumber(this.health.monster, 0, 100);
+      return {
+        me:  this.health.me,
+        monster: this.health.monster
+      }
+    }
+  },
   methods: {
     attack: function() {
       this.health.me -= AppUtis.randNumber(10);
       this.health.monster -= AppUtis.randNumber(10);
+    },
+    heal: function() {
+      this.health.me += AppUtis.randNumber(10);
+      this.health.me -= AppUtis.randNumber(10);
     }
   }
 })
